6b53623816292840156e0618a800b0dbbf5a8fd1
alexburn17 <pburnham@uvm.edu>
2017-02-25 10:16:33 -0500

    added data to script

--- NearFar/NearFarAnalysis.R
+++ NearFar/NearFarAnalysis.R
@@ -41,9 +41,26 @@ number_bees <- 1
 cDNA_eff <- 0.1
 rxn_vol <- 3
 
+
 # create column for genome copies per bee:
 NFdata$genome_copbee <- ((((((NFdata$QuantityMean / cDNA_eff) / rxn_vol) * NFdata$dilFactor) * eluteRNA) / crude_extr) * total_extr_vol) / number_bees
 
+# pull only actin values out of dataframe
+ActinOnly <- NFdata[which(NFdata$Virus=="ACTIN"),]
+
+# create DF of ACTIN genome copies and lab ID:
+ActinDF <- data.frame(ActinOnly$Virus, ActinOnly$genome_copbee)
+colnames(ActinDF) <- c("Virus", "ACT_genome_copbee")
+
+# merge ACTIN dataframe with main dataframe:
+#Need rownames and all.x=TRUE because data frames are different sizes.
+NFdata <- merge(NFdata, ActinDF, by="sample_name")
+
+# find mean of all ACTIN values:
+ActinMean <- mean(ActinOnly$genome_copbee, na.rm = TRUE)
+
+# create column for normalized genome copies per bee:
+NFdata$norm_genome_copbee <- (NFdata$genome_copbee/NFdata$ACT_genome_copbee)*ActinMean
 
 
 
